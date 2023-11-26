a115ae23cdcd423b299d37fdf6427cf5bbe0eed1
Li_2023-9-13 <1476021453@qq.com>
2023-11-26 23:32:21 +0800

    2023.11.26力扣网刷题 1.各位相加——数学、数论、模拟——简单

---
+++ 23.11.26_leetcode/23.11.26_leetcode/practice.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//2023.11.26力扣网刷题
+//各位相加——数学、数论、模拟——简单
+//给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。返回这个结果。
+//示例 1:
+//输入: num = 38
+//输出 : 2
+//解释 : 各位相加的过程为：
+//38 -- > 3 + 8 -- > 11
+//11 -- > 1 + 1 -- > 2
+//由于 2 是一位数，所以返回 2。
+//示例 2:
+//输入: num = 0
+//输出 : 0
+//提示：
+//0 <= num <= 231 - 1
+//进阶：你可以不使用循环或者递归，在 O(1) 时间复杂度内解决这个问题吗？
+
+int addDigits(int num) {
+	int a = 0;
+	for (num; num > 0; num /= 10)
+		a += num % 10;
+	int b = 0;
+	for (a; a > 0; a /= 10)
+		b += a % 10;
+	int sum = 0;
+	for (b; b > 0; b /= 10)
+		sum += b % 10;
+	return sum;
+}
+int main()
+{
+	int num = 0;
+	while (scanf("%d", &num) == 1)
+	{
+		int ret = addDigits(num);
+		printf("%d\n", ret);
+	}
+	return 0;
+}
