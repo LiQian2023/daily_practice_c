496e2df4dc11a6c0400984f4d0351b257af28d78
Li_2023-9-13 <1476021453@qq.com>
2023-12-12 20:06:56 +0800

    2023.12.12力扣网刷题 搜索插入位置——数组、二分查找——简单

---
+++ 23.12.12_leetcode/23.12.12_leetcode/practice.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//2023.12.12力扣网刷题
+// 搜索插入位置——数组、二分查找——简单
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+//如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+//请必须使用时间复杂度为 O(log n) 的算法。
+//示例 1:
+//输入: nums = [1, 3, 5, 6], target = 5
+//输出 : 2
+//示例 2 :
+//输入 : nums = [1, 3, 5, 6], target = 2
+//输出 : 1
+//示例 3 :
+//输入 : nums = [1, 3, 5, 6], target = 7
+//输出 : 4
+//提示 :
+//1 <= nums.length <= 10^4
+//- 104 <= nums[i] <= 10^4
+//nums 为 无重复元素 的 升序 排列数组
+//- 10^4 <= target <= 10^4
+int searchInsert(int* nums, int numsSize, int target) {
+	int left = 0;
+	int right = numsSize - 1;
+	int mid = 0;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if (nums[mid] < target)
+			left = mid + 1;
+		else if (nums[mid] > target)
+			right = mid - 1;
+		else
+			break;
+	}
+	if (left > right && nums[mid] < target)
+		return mid + 1;
+	else if (left > right && nums[mid] > target)
+		return mid;
+	return mid;
+}
+int main()
+{
+	int arr[10000] = { 0 };
+	int n = 0;
+	while (scanf("%d", &n) == 1)
+	{
+		for (int i = 0; i < n; i++)
+			scanf("%d", &arr[i]);
+		int k = 0;
+		scanf("%d", &k);
+		int ret = searchInsert(arr, n, k);
+		printf("%d\n", ret);
+	}
+	return 0;
+}
