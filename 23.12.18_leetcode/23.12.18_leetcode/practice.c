9e81bce3212752493deb7d79d520a97cd7ffe324
Li_2023-9-13 <1476021453@qq.com>
2023-12-18 21:42:44 +0800

    2023.12.18力扣网刷题 猜数字大小——二分查找、交互——简单

---
+++ 23.12.18_leetcode/23.12.18_leetcode/practice.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//2023.12.18力扣网刷题
+//猜数字大小——二分查找、交互——简单
+//猜数字游戏的规则如下：
+//每轮游戏，我都会从 1 到 n 随机选择一个数字。 请你猜选出的是哪个数字。
+//如果你猜错了，我会告诉你，你猜测的数字比我选出的数字是大了还是小了。
+//你可以通过调用一个预先定义好的接口 int guess(int num) 来获取猜测结果，返回值一共有 3 种可能的情况（ - 1，1 或 0）：
+//- 1：我选出的数字比你猜的数字小 pick < num
+//	1：我选出的数字比你猜的数字大 pick > num
+//	0：我选出的数字和你猜的数字一样。恭喜！你猜对了！pick == num
+//	返回我选出的数字。
+//	示例 1：
+//	输入：n = 10, pick = 6
+//	输出：6
+//	示例 2：
+//	输入：n = 1, pick = 1
+//	输出：1
+//	示例 3：
+//	输入：n = 2, pick = 1
+//	输出：1
+//	示例 4：
+//	输入：n = 2, pick = 2
+//	输出：2
+//	提示：
+//	1 <= n <= 231 - 1
+//	1 <= pick <= n
+
+#define pick 2
+int guess(int num)
+{
+	if (num < pick)
+		return 1;
+	else if (num == pick)
+		return 0;
+	else
+		return -1;
+}
+
+int guessNumber(int n) {
+	int l = 1, r = n;
+	while (l < r)
+	{
+		int m = l + (r - l) / 2;//避免溢出
+		if (guess(m) < 0)
+			r = m - 1;
+		else if (guess(m) > 0)
+			l = m + 1;
+		else
+			return m;
+	}
+	return l;
+}
+int main()
+{
+	int n = 0;
+	while (scanf("%d", &n) == 1)
+	{
+		int ret = guessNumber(n);
+		printf("%d\n", ret);
+	}
+	return 0;
+}
