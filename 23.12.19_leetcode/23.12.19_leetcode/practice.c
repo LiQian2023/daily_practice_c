7ed70981179dc3d7776855ef3add8a986322769e
Li_2023-9-13 <1476021453@qq.com>
2023-12-19 19:03:37 +0800

    2023.12.19力扣网刷题 第一个错误的版本——二分查找、交互——简单

---
+++ 23.12.19_leetcode/23.12.19_leetcode/practice.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+
+//2023.12.19力扣网刷题
+//第一个错误的版本——二分查找、交互——简单
+//你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+//假设你有 n 个版本[1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+//你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+//示例 1：
+//输入：n = 5, bad = 4
+//输出：4
+//解释：
+//调用 isBadVersion(3) -> false
+//调用 isBadVersion(5) -> true
+//调用 isBadVersion(4) -> true
+//所以，4 是第一个错误的版本。
+//示例 2：
+//输入：n = 1, bad = 1
+//输出：1
+//提示：
+//1 <= bad <= n <= 2^31 - 1
+#define bad 4
+bool isBadVersion(int version)
+{
+	if (version == bad)
+		return true;
+	else
+		return false;
+}
+
+int firstBadVersion(int n) {
+	int l = 1, r = n;
+	while (l < r)
+	{
+		int m = l + (r - l) / 2;
+		if (isBadVersion(m))
+			r = m ;
+		else
+			l = m + 1;
+	}
+	return l;
+}
+int main()
+{
+	int n = 0;
+	while (scanf("%d", &n) == 1)
+	{
+		int ret = firstBadVersion(n);
+		printf("%d\n", ret);
+	}
+	return 0;
+}
