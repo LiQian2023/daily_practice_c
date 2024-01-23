0f0d6f1831069cc7bea0c90f1b14fdbff234cda6
Li_2023-9-13 <1476021453@qq.com>
2024-01-23 20:53:30 +0800

    2024.01.23力扣网刷题 相交链表——哈希表、链表、双指针——简单

---
+++ 24.01.23_leetcode/24.01.23_leetcode/practice.c
@@ -0,0 +1,161 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+//2024.01.23力扣网刷题
+//相交链表——哈希表、链表、双指针——简单
+//给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+//图示两个链表在节点 c1 开始相交：
+//题目数据 保证 整个链式结构中不存在环。
+//注意，函数返回结果后，链表必须 保持其原始结构 。
+//自定义评测：
+//评测系统 的输入如下（你设计的程序 不适用 此输入）：
+//intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+//listA - 第一个链表
+//listB - 第二个链表
+//skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+//skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+//评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+//示例 1：
+//输入：intersectVal = 8, listA = [4, 1, 8, 4, 5], listB = [5, 6, 1, 8, 4, 5], skipA = 2, skipB = 3
+//输出：Intersected at '8'
+//解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+//从各自的表头开始算起，链表 A 为[4, 1, 8, 4, 5]，链表 B 为[5, 6, 1, 8, 4, 5]。
+//在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+//— 请注意相交节点的值不为 1，因为在链表 A 和链表 B 之中值为 1 的节点(A 中第二个节点和 B 中第三个节点) 是不同的节点。换句话说，它们在内存中指向两个不同的位置，而链表 A 和链表 B 中值为 8 的节点(A 中第三个节点，B 中第四个节点) 在内存中指向相同的位置。
+//示例 2：
+//输入：intersectVal = 2, listA = [1, 9, 1, 2, 4], listB = [3, 2, 4], skipA = 3, skipB = 1
+//输出：Intersected at '2'
+//解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+//从各自的表头开始算起，链表 A 为[1, 9, 1, 2, 4]，链表 B 为[3, 2, 4]。
+//在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+//示例 3：
+//输入：intersectVal = 0, listA = [2, 6, 4], listB = [1, 5], skipA = 3, skipB = 2
+//输出：null
+//解释：从各自的表头开始算起，链表 A 为[2, 6, 4]，链表 B 为[1, 5]。
+//由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+//这两个链表不相交，因此返回 null 。
+//提示：
+//listA 中节点数目为 m
+//listB 中节点数目为 n
+//1 <= m, n <= 3 * 10^4
+//1 <= Node.val <= 10^5
+//0 <= skipA <= m
+//0 <= skipB <= n
+//如果 listA 和 listB 没有交点，intersectVal 为 0
+//如果 listA 和 listB 有交点，intersectVal == listA[skipA] == listB[skipB]
+//进阶：你能否设计一个时间复杂度 O(m + n) 、仅用 O(1) 内存的解决方案？
+
+struct ListNode {
+	int val;
+	struct ListNode* next;
+};
+//初始化
+void InitList(struct ListNode** L) {
+	assert(L);
+	*L = NULL;
+}
+//尾插法创建链表
+bool List_TailInsert(struct ListNode** L) {
+	if (!L)
+		return false;
+	struct ListNode* l, * r;
+	int x = 0;
+	while (scanf("%d", &x) == 1) {
+		if (!(*L))
+		{
+			*L = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+			if (!(*L))
+				return false;
+			(*L)->val = x;
+			(*L)->next = NULL;
+			l = *L;
+		}
+		else {
+			r = (struct ListNode*)calloc(1, sizeof(struct ListNode));
+			if (!r)
+				return false;
+			r->val = x;
+			r->next = l->next;
+			l->next = r;
+			l = r;
+		}
+	}
+	getchar();
+	return true;
+}
+//法一：双指针——相遇问题
+struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* headB) {
+	if (!headA || !headB)
+		return NULL;
+	struct ListNode* pa = headA, * pb = headB;
+	int len1 = 0, len2 = 0;
+	while (pa) {
+		len1++;//记录A的表长
+		pa = pa->next;
+	}
+	while (pb) {
+		len2++;//记录B的表长
+		pb = pb->next;
+	}
+	pa = headA, pb = headB;
+	//双指针遍历A和B
+	for (int i = 0; i < len1 + len2; i++) {
+		//先判断pa与pb是否相等，不相等再开始遍历
+		if (pa == pb)
+			break;
+		//当A遍历完pa开始遍历B
+		if (!pa)
+			pa = headB;
+		//当B遍历完，开始遍历A
+		if (!pb)
+			pb = headA;
+		//遍历各自链表
+		pa = pa->next;
+		pb = pb->next;
+		
+	}
+	if (pa && pb)
+		return pa;
+	return NULL;
+}
+void Print_List(struct ListNode* L) {
+	assert(L);
+	printf("打印链表>:");
+	while (L) {
+		printf("%d->", L->val);
+		L = L->next;
+	}
+	printf("NULL\n");
+}
+int main() {
+	struct ListNode* L1, * L2, * L3;
+	InitList(&L1);
+	InitList(&L2);
+	int k = 0;
+	while (scanf("%d", &k) == 1) {
+		if (List_TailInsert(&L1) && List_TailInsert(&L2)) {
+			Print_List(L1);
+			Print_List(L2);
+		}
+		else {
+			printf("创建失败\n");
+		}
+		struct ListNode* ret = getIntersectionNode(L1, L2);
+		Print_List(ret);
+		if (ret->val == k) {
+			printf("Intersected at %d\n", ret->val);
+		}
+		else {
+			printf("null\n");
+		}
+		free(L1);
+		free(L2);
+		L1 = NULL;
+		L2 = NULL;
+	}
+	return 0;
+}
