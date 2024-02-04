2e7a096c3fc0ccd49dbf74a416a28ec277128a0f
Li_2023-9-13 <1476021453@qq.com>
2024-02-05 06:16:22 +0800

    2024.02.05力扣网刷题 题目测试

---
+++ 24.02.05_leetcode/24.02.05_leetcode/practice_test.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "pratice.h"
+
+
+int main() {
+	//test1();
+	return 0;
+}
---
+++ 24.02.05_leetcode/24.02.05_leetcode/pratice.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+
+//2024.02.05力扣网刷题
+//棒球比赛——栈、数组、模拟——简单
+//题目一测试函数
+void test1();
+//2024.02.05力扣网刷题
+//柠檬水找零——贪心、数组——简单
+//题目二测试函数
+void test2();
+//2024.02.05力扣网刷题
+//三角形的最大周长——贪心、数组、数学、排序——简单
+//题目三测试函数
+void test3();
+//2024.02.05力扣网刷题
+//重新排列数组——数组——简单
+//题目四测试函数
+void test4();
+
