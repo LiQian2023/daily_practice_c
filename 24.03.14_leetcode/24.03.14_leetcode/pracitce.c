e7b4e34c6f3b97e0325a7b48f3e7d2e187415992
Li_2023-9-13 <1476021453@qq.com>
2024-03-14 23:54:11 +0800

    2024.03.14力扣网刷题 丑数 II——哈希表、数字、动态规划、堆（优先队列）——中等

---
+++ 24.03.14_leetcode/24.03.14_leetcode/pracitce.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+
+//2024.03.14力扣网刷题
+//丑数 II——哈希表、数字、动态规划、堆（优先队列）——中等
+//给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+//丑数 就是质因子只包含 2、3 和 5 的正整数。
+//示例 1：
+//输入：n = 10
+//输出：12
+//解释：[1, 2, 3, 4, 5, 6, 8, 9, 10, 12] 是由前 10 个丑数组成的序列。
+//示例 2：
+//输入：n = 1
+//输出：1
+//解释：1 通常被视为丑数。
+//提示：
+//1 <= n <= 1690
+
+bool isUgly(int n) {
+	while (n >= 2 && n % 2 == 0) {
+		n /= 2;
+	}
+	while (n >= 3 && n % 3 == 0) {
+		n /= 3;
+	}
+	while (n >= 5 && n % 5 == 0) {
+		n /= 5;
+	}
+	if (n < 5)
+		return true;
+	return false;
+}
+
+int nthUglyNumber(int n) {
+	int* num = (int*)calloc(n, sizeof(int));
+	assert(num);
+	int i = 0;
+	for (int x = 1; i < n; x++) {
+		if (isUgly(x))
+			num[i++] = x;
+	}
+	return num[n - 1];
+}
