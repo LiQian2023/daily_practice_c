ca3a40d7d74bb2713eef4dc4bbc9a58f61a17a87
Li_2023-9-13 <1476021453@qq.com>
2024-04-16 05:58:50 +0800

    2024.02.17力扣网刷题 两数相加 II——栈、链表、数学——中等 2024.04.16完成解题——解题方法——法一：新链表、法二：顺序表 链表头文件

--- 24.02.17_leetcode/24.02.17_leetcode/practice.h
+++ 24.02.17_leetcode/24.02.17_leetcode/practice.h
@@ -9,3 +9,8 @@ struct ListNode {
 	int val;
 	struct ListNode* next;
 };
+
+void InitList(struct ListNode** L);
+void TailInsert(struct ListNode** L);
+void Destroy(struct ListNode** L);
+void Print(struct ListNode* L);
