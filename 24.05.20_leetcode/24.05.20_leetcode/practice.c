b3af48a8a36b3eae44bc85a3f9c8dfc764c8d586
Li_2023-9-13 <1476021453@qq.com>
2024-05-20 21:39:04 +0800

    2024.05.20力扣网刷题 拆分数位后四位数字的最小和——贪心、数学、排序——简单

---
+++ 24.05.20_leetcode/24.05.20_leetcode/practice.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//2024.05.20力扣网刷题
+//拆分数位后四位数字的最小和——贪心、数学、排序——简单
+//给你一个四位 正 整数 num 。请你使用 num 中的 数位 ，将 num 拆成两个新的整数 new1 和 new2 。new1 和 new2 中可以有 前导 0 ，且 num 中 所有 数位都必须使用。
+//比方说，给你 num = 2932 ，你拥有的数位包括：两个 2 ，一个 9 和一个 3 。一些可能的[new1, new2] 数对为[22, 93]，[23, 92]，[223, 9] 和[2, 329] 。
+//请你返回可以得到的 new1 和 new2 的 最小 和。
+//示例 1：
+//输入：num = 2932
+//输出：52
+//解释：可行的[new1, new2] 数对为[29, 23] ，[223, 9] 等等。
+//最小和为数对[29, 23] 的和：29 + 23 = 52 。
+//示例 2：
+//输入：num = 4009
+//输出：13
+//解释：可行的[new1, new2] 数对为[0, 49] ，[490, 0] 等等。
+//最小和为数对[4, 9] 的和：4 + 9 = 13 。
+//提示：
+//1000 <= num <= 9999
+
+int cmp(const void* p1, const void* p2) {
+	return *(int*)p1 - *(int*)p2;
+}
+int minimumSum(int num) {
+	int arr[4] = { 0 };
+	for (int i = 0; num; num /= 10)
+		arr[i++] = num % 10;
+	qsort(arr, 4, sizeof(int), cmp);
+	int num1 = arr[0] * 10 + arr[3];
+	int num2 = arr[1] * 10 + arr[2];
+	return num1 + num2;
+}
