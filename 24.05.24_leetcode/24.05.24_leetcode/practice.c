8501948c5ab7dccfdf40c2d73923f484909c3d87
Li_2023-9-13 <1476021453@qq.com>
2024-05-25 00:18:25 +0800

    2024.05.24力扣网刷题 左叶子之和——树、深度优先搜索、广度优先搜索、二叉树——简单 方法一：广度优先搜索——层序遍历+排序——队列+顺序表实现 方法二：深度优先搜索——分治思想——递归实现

--- 24.05.24_leetcode/24.05.24_leetcode/practice.c
+++ 24.05.24_leetcode/24.05.24_leetcode/practice.c
@@ -16,6 +16,11 @@
 //	节点数在[1, 1000] 范围内
 //	- 1000 <= Node.val <= 1000
 
+//广度优先搜索
+int cmp(const void* p1, const void* p2) {
+	return *(int*)p1 - *(int*)p2;
+}
+
 int sumOfLeftLeaves(struct TreeNode* root) {
 	if (!root->left && !root->right)
 		return 0;
@@ -25,7 +30,9 @@ int sumOfLeftLeaves(struct TreeNode* root) {
 	int sum = 0;
 	struct TreeNode* p = root;
 	arr[0] = p;//首元素入队
-	int num[1002] = { 0 };
+	int num[2048] = { 0 };
+	num[0] = 1001;
+	num[1] = 1001;
 	int i = 2;
 	while (f != r) {
 		p = arr[f++];
@@ -33,21 +40,34 @@ int sumOfLeftLeaves(struct TreeNode* root) {
 		if (p->left) {
 			arr[r++] = p->left;
 			r %= 500;
-			if(p->left->left)
+			if (!p->left->left && !p->left->right)
+				num[i++] = p->left->val;
+			else
+				num[i++] = 1001;
 		}
+		else
+			num[i++] = 1001;
 		if (p->right) {
 			arr[r++] = p->right;
 			r %= 500;
+			num[i++] = 1001;
 		}
+		else
+			num[i++] = 1001;
 	}
-	for (int j = 2; j < i; j++) {
-		if (2 * j <= i && num[2 * j] == 1001) {
-			if (2 * j + 1 <= i && num[2 * j + 1] == 1001)
-				sum += num[j];
-		}
-		else if (2 * j > i && num[2 * j] != 1001) {
-			sum += num[j];
-		}
-	}
+	qsort(num, i, sizeof(int), cmp);
+	for (int j = 0; num[j] != 1001; j++)
+		sum += num[j];
 	return sum;
+}
+
+int sumOfLeftLeaves(struct TreeNode* root) {
+	if (!root)
+		return 0;
+	int  ans = 0;
+	if (root->left && !root->left->left && !root->left->right)
+		ans = root->left->val;
+	int l = sumOfLeftLeaves(root->left);
+	int r = sumOfLeftLeaves(root->right);
+	return l + r + ans;
 }
