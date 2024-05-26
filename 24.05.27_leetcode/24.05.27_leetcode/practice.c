c2a3cec893b717c960297eaea283784d3b3b067e
Li_2023-9-13 <1476021453@qq.com>
2024-05-27 00:49:28 +0800

    2024.05.27力扣网刷题 完全二叉树的节点个数——位运算、树、二分查找、二叉树——简单 分治

---
+++ 24.05.27_leetcode/24.05.27_leetcode/practice.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "BiTree.h"
+
+//2024.05.27力扣网刷题
+//完全二叉树的节点个数——位运算、树、二分查找、二叉树——简单
+//给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+//完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。
+//若最底层为第 h 层，则该层包含 1~2h 个节点。
+//示例 1：
+//输入：root = [1, 2, 3, 4, 5, 6]
+//输出：6
+//示例 2：
+//输入：root = []
+//输出：0
+//示例 3：
+//输入：root = [1]
+//输出：1
+//提示：
+//树中节点的数目范围是[0, 5 * 10^4]
+//0 <= Node.val <= 5 * 10^4
+//题目数据保证输入的树是 完全二叉树
+//进阶：遍历树来统计节点是一种时间复杂度为 O(n) 的简单解决方案。你可以设计一个更快的算法吗？
+
+int countNodes(struct TreeNode* root) {
+	if (!root)
+		return 0;
+	int l = countNodes(root->left);
+	int r = countNodes(root->right);
+	return l + r + 1;
+}
+
+int countNodes(struct TreeNode* root) {
+	return !root ? 0 : countNodes(root->left) + countNodes(root->right) + 1;
+}
