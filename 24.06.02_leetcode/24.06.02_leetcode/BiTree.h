9f4007911d18768a43504f7360c52c9fd9e72bc3
Li_2023-9-13 <1476021453@qq.com>
2024-06-02 20:55:56 +0800

    2024.06.02力扣网刷题 二叉树的坡度——树、深度优先搜索、二叉树——简单 二叉树头文件

---
+++ 24.06.02_leetcode/24.06.02_leetcode/BiTree.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
