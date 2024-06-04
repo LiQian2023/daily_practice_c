e10b9e5b9bb9f4b02c08727e83b33aeb950f5c9c
Li_2023-9-13 <1476021453@qq.com>
2024-06-04 16:07:25 +0800

    2024.06.04力扣网刷题 计算布尔二叉树的值——树、深度优先搜索、二叉树——简单 二叉树头文件

---
+++ 24.06.04_leetcode/24.06.04_leetcode/BiTree.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
