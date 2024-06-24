23837ba5c87dc8efb95c16199f137ceb00b7b717
Li_2023-9-13 <1476021453@qq.com>
2024-06-24 13:23:56 +0800

    2024.06.23力扣网刷题 翻转二叉树——树、深度优先搜索、广度优先搜索、二叉树——简单 二叉树头文件

--- 24.06.23_leetcode/24.06.23_leetcode/BiTree.h
+++ 24.06.23_leetcode/24.06.23_leetcode/BiTree.h
@@ -1,1 +1,13 @@
 #pragma once
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
