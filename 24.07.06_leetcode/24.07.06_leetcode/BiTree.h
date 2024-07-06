4307c7169e8226637a9ca573efd9a493a63706f9
Li_2023-9-13 <1476021453@qq.com>
2024-07-06 23:24:43 +0800

    2024.07.06力扣网刷题 使所有元素都可以被 3 整除的最少操作数——数组、数学——简单 二叉树头文件

---
+++ 24.07.06_leetcode/24.07.06_leetcode/BiTree.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
