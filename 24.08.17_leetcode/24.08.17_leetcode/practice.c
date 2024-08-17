eee8b6bed2005f5466598d62b59693bde38fac58
Li_2023-9-13 <1476021453@qq.com>
2024-08-17 10:53:49 +0800

    2024.08.17力扣网刷题 装满杯子需要的最短总时长——贪心、数组、排序、堆（优先队列）——简单 方法一：数学

---
+++ 24.08.17_leetcode/24.08.17_leetcode/practice.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+//2024.08.17力扣网刷题
+//装满杯子需要的最短总时长——贪心、数组、排序、堆（优先队列）——简单
+//现有一台饮水机，可以制备冷水、温水和热水。每秒钟，可以装满 2 杯 不同 类型的水或者 1 杯任意类型的水。
+//给你一个下标从 0 开始、长度为 3 的整数数组 amount ，
+//其中 amount[0]、amount[1] 和 amount[2] 分别表示需要装满冷水、温水和热水的杯子数量。
+//返回装满所有杯子所需的 最少 秒数。
+//示例 1：
+//输入：amount = [1, 4, 2]
+//输出：4
+//解释：下面给出一种方案：
+//第 1 秒：装满一杯冷水和一杯温水。
+//第 2 秒：装满一杯温水和一杯热水。
+//第 3 秒：装满一杯温水和一杯热水。
+//第 4 秒：装满一杯温水。
+//可以证明最少需要 4 秒才能装满所有杯子。
+//示例 2：
+//输入：amount = [5, 4, 4]
+//输出：7
+//解释：下面给出一种方案：
+//第 1 秒：装满一杯冷水和一杯热水。
+//第 2 秒：装满一杯冷水和一杯温水。
+//第 3 秒：装满一杯冷水和一杯温水。
+//第 4 秒：装满一杯温水和一杯热水。
+//第 5 秒：装满一杯冷水和一杯热水。
+//第 6 秒：装满一杯冷水和一杯温水。
+//第 7 秒：装满一杯热水。
+//示例 3：
+//输入：amount = [5, 0, 0]
+//输出：5
+//解释：每秒装满一杯冷水。
+//提示：
+//amount.length == 3
+//0 <= amount[i] <= 100
+void Swap(int* a, int* b) {
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int fillCups(int* amount, int amountSize) {
+	int max = amount[0];
+	int mid = amount[1];
+	int min = amount[2];
+	if (max < mid) {
+		Swap(&max, &mid);
+	}
+	if (max < min) {
+		Swap(&max, &min);
+	}
+	if (mid < min) {
+		Swap(&mid, &min);
+	}
+	return mid + min > max ? (max + mid + min) / 2 + (max + mid + min) % 2 : max;
+}
