c03fd93a6351e1e593c6ef8766468ec3939c885b
Li_2023-9-13 <1476021453@qq.com>
2024-08-18 17:41:46 +0800

    2024.08.14力扣网刷题 按奇偶性交换后的最大数字——排序、堆（优先队列）——简单 方法一：选择排序

---
+++ 24.08.14_leetcode/24.08.14_leetcode/practice.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//2024.08.14力扣网刷题
+//按奇偶性交换后的最大数字——排序、堆（优先队列）——简单
+//给你一个正整数 num 。你可以交换 num 中 奇偶性 相同的任意两位数字（即，都是奇数或者偶数）。
+//返回交换 任意 次之后 num 的 最大 可能值。
+//示例 1：
+//输入：num = 1234
+//输出：3412
+//解释：交换数字 3 和数字 1 ，结果得到 3214 。
+//交换数字 2 和数字 4 ，结果得到 3412 。
+//注意，可能存在其他交换序列，但是可以证明 3412 是最大可能值。
+//注意，不能交换数字 4 和数字 1 ，因为它们奇偶性不同。
+//示例 2：
+//输入：num = 65875
+//输出：87655
+//解释：交换数字 8 和数字 6 ，结果得到 85675 。
+//交换数字 5 和数字 7 ，结果得到 87655 。
+//注意，可能存在其他交换序列，但是可以证明 87655 是最大可能值。
+//提示：
+//1 <= num <= 10^9
+
+// 方法一：选择排序
+void Swap(int* p1, int* p2) {
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+int largestInteger(int num) {
+	int nums[10] = { 0 };
+	int len = 0;
+	while (num) {
+		nums[len++] = num % 10;
+		num /= 10;
+	}
+	int l = 0, r = len - 1;
+	while (l < r) {
+		Swap(&nums[l], &nums[r]);
+		l++, r--;
+	}
+	for (int i = 0; i < len - 1; i++) {
+		for (int j = i + 1; j < len; j++) {
+			if (nums[i] < nums[j] && nums[i] % 2 == nums[j] % 2) {
+				Swap(&nums[i], &nums[j]);
+			}
+		}
+	}
+	for (int i = 0; i < len; i++) {
+		num *= 10;
+		num += nums[i];
+	}
+	return num;
+}
