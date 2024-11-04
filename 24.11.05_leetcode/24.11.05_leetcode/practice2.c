1e02df1c08ae47a7ec87471848ded4526381e814
Li_2023-9-13 <1476021453@qq.com>
2024-11-05 04:10:57 +0800

    2024.11.05力扣网刷题 求出加密整数的和——数组、数学——简单 方法一：数组

---
+++ 24.11.05_leetcode/24.11.05_leetcode/practice2.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//2024.11.05力扣网刷题
+//求出加密整数的和——数组、数学——简单
+//给你一个整数数组 nums ，数组中的元素都是 正 整数。
+//定义一个加密函数 encrypt ，encrypt(x) 将一个整数 x 中 每一个 数位都用 x 中的 最大 数位替换。比方说 encrypt(523) = 555 且 encrypt(213) = 333 。
+//请你返回数组中所有元素加密后的 和 。
+//示例 1：
+//输入：nums = [1, 2, 3]
+//输出：6
+//解释：加密后的元素位[1, 2, 3] 。加密元素的和为 1 + 2 + 3 == 6 。
+//示例 2：
+//输入：nums = [10, 21, 31]
+//输出：66
+//解释：加密后的元素为[11, 22, 33] 。加密元素的和为 11 + 22 + 33 == 66 。
+//提示：
+//1 <= nums.length <= 50
+//1 <= nums[i] <= 1000
+
+int sumOfEncryptedInt(int* nums, int numsSize) {
+	int ans = 0;
+	for (int i = 0; i < numsSize; i++) {
+		int max = nums[i] % 10;
+		int n = 0;
+		//找最大值
+		while (nums[i]) {
+			if (nums[i] % 10 > max)
+				max = nums[i] % 10;
+			nums[i] /= 10;
+			n += 1;
+		}
+		//替换元素
+		while (n) {
+			nums[i] *= 10;
+			nums[i] += max;
+			n -= 1;
+		}
+		ans += nums[i];
+	}
+	return ans;
+}
