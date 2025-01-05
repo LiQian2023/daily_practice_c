77f0003e2ccb66fd014a50d758a69425aebc8672
Li_2023-9-13 <1476021453@qq.com>
2025-01-05 09:42:29 +0800

    2025.01.05力扣网刷题 分式化简——数组、数学、数论、模拟——简单 方法一：数学

---
+++ 25.01.05_leetcode/25.01.05_leetcode/practice.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+//2025.01.05力扣网刷题
+//分式化简——数组、数学、数论、模拟——简单
+//有一个同学在学习分式。他需要将一个连分数化成最简分数，你能帮助他吗？
+//连分数是形如上图的分式。在本题中，所有系数都是大于等于0的整数。
+//输入的cont代表连分数的系数（cont[0]代表上图的a0，以此类推）。返回一个长度为2的数组[n, m]，使得连分数的值等于n / m，且n, m最大公约数为1。
+//示例 1：
+//输入：cont = [3, 2, 0, 2]
+//输出：[13, 4]
+//解释：原连分数等价于3 + (1 / (2 + (1 / (0 + 1 / 2))))。注意[26, 8], [-13, -4]都不是正确答案。
+//示例 2：
+//输入：cont = [0, 0, 3]
+//输出：[3, 1]
+//解释：如果答案是整数，令分母为1即可。
+//限制：
+//cont[i] >= 0
+//1 <= cont的长度 <= 10
+//cont最后一个元素不等于0
+//答案的n, m的取值都能被32位int整型存下（即不超过2 ^ 31 - 1）。
+
+int* fraction(int* cont, int contSize, int* returnSize) {
+	*returnSize = 2;
+	int* ans = (int*)calloc(2, sizeof(int));
+	assert(ans);
+	int a = cont[contSize - 1] * 1.0;
+	int b = 1.0;
+	for (int i = contSize - 2; i >= 0; i--) {
+		int tmp = b;
+		b = a;
+		a = cont[i] * a + tmp;
+	}
+	ans[0] = b;
+	ans[1] = a;
+	return ans;
+}
