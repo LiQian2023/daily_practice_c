351a2282fd66b1a2dda5fd82f210fd5736573a5d
Li_2023-9-13 <1476021453@qq.com>
2025-02-13 14:17:07 +0800

    2025.02.13力扣网刷题 验证外星语词典——数组、哈希表、字符串——简单 方法一：哈希表

---
+++ 25.02.13_leetcode/25.02.13_leetcode/practice.c
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+
+//2025.02.13力扣网刷题
+//验证外星语词典——数组、哈希表、字符串——简单
+//某种外星语也使用英文小写字母，但可能顺序 order 不同。字母表的顺序（order）是一些小写字母的排列。
+//给定一组用外星语书写的单词 words，以及其字母表的顺序 order，只有当给定的单词在这种外星语中按字典序排列时，返回 true；否则，返回 false。
+//示例 1：
+//输入：words = ["hello", "leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+//输出：true
+//解释：在该语言的字母表中，'h' 位于 'l' 之前，所以单词序列是按字典序排列的。
+//示例 2：
+//输入：words = ["word", "world", "row"], order = "worldabcefghijkmnpqstuvxyz"
+//输出：false
+//解释：在该语言的字母表中，'d' 位于 'l' 之后，那么 words[0] > words[1]，因此单词序列不是按字典序排列的。
+//示例 3：
+//输入：words = ["apple", "app"], order = "abcdefghijklmnopqrstuvwxyz"
+//输出：false
+//解释：当前三个字符 "app" 匹配时，第二个字符串相对短一些，然后根据词典编纂规则 "apple" > "app"，因为 'l' > '∅'，其中 '∅' 是空白字符，定义为比任何其他字符都小（更多信息）。
+//提示：
+//1 <= words.length <= 100
+//1 <= words[i].length <= 20
+//order.length == 26
+//在 words[i] 和 order 中的所有字符都是英文小写字母。
+
+bool isAlienSorted(char** words, int wordsSize, char* order) {
+	int hash[26] = { 0 };
+	for (int i = 0; order[i]; i++) {
+		int key = order[i] - 'a';
+		hash[key] = i;
+	}
+	for (int i = 0; i < wordsSize - 1; i++) {
+		int len1 = strlen(words[i]);
+		int len2 = strlen(words[i + 1]);
+		int j = 0;
+		for (j; j < len1 && j < len2; j++) {
+			int key1 = words[i][j] - 'a';
+			int key2 = words[i + 1][j] - 'a';
+			if (hash[key1] - hash[key2] > 0) {
+				return false;
+			}
+			else if (hash[key1] - hash[key2] < 0) {
+				break;
+			}
+		}
+		if (j == len1 || j == len2) {
+			if (len1 > len2) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
