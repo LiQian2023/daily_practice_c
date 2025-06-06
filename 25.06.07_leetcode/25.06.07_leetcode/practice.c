cf2c99f58fe1f5d1a8aa4eb8b65e48eaaa553632
Li_2023-9-13 <1476021453@qq.com>
2025-06-07 07:56:40 +0800

    2025.06.07力扣网刷题 数位和等于下标的最小下标——数组、数学——简单 方法一：哈希表

---
+++ 25.06.07_leetcode/25.06.07_leetcode/practice.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+
+//2025.06.07力扣网刷题
+//数位和等于下标的最小下标——数组、数学——简单
+//给你一个整数数组 nums 。
+//返回满足 nums[i] 的数位和（每一位数字相加求和）等于 i 的 最小 下标 i 。
+//如果不存在满足要求的下标，返回 - 1 。
+//示例 1：
+//输入：nums = [1, 3, 2]
+//输出：2
+//解释：
+//nums[2] = 2，其数位和等于 2 ，与其下标 i = 2 相等。因此，输出为 2 。
+//示例 2：
+//输入：nums = [1, 10, 11]
+//输出：1
+//解释：
+//nums[1] = 10，其数位和等于 1 + 0 = 1，与其下标 i = 1 相等。
+//nums[2] = 11，其数位和等于是 1 + 1 = 2，与其下标 i = 2 相等。
+//由于下标 1 是满足要求的最小下标，输出为 1 。
+//示例 3：
+//输入：nums = [1, 2, 3]
+//输出： - 1
+//解释：
+//由于不存在满足要求的下标，输出为 - 1 。
+//提示：
+//1 <= nums.length <= 100
+//0 <= nums[i] <= 1000
+
+int smallestIndex(int* nums, int numsSize) {
+	int* hash = (int*)calloc(30, sizeof(int));
+	assert(hash);
+	for (int i = 0; i < numsSize; i++) {
+		int key = 0;
+		while (nums[i]) {
+			key += nums[i] % 10;
+			nums[i] /= 10;
+		}
+		if (key == i) {
+			hash[key] += 1;
+		}
+	}
+	int ans = -1;
+	for (int i = 0; i < 30; i++) {
+		if (hash[i]) {
+			ans = i;
+			break;
+		}
+	}
+	free(hash);
+	return ans;
+}
