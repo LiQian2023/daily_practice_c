192014c826b386d4860da829f6f4645c02a0bc13
Li_2023-9-13 <1476021453@qq.com>
2025-08-05 21:02:30 +0800

    2025.08.05力扣网刷题 水果成篮 II——线段树、数组、二分查找、有序集合、模拟、第440场周赛——简单 方法一：模拟

---
+++ 25.08.05_leetcode/25.08.05_leetcode/practice.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+//2025.08.05力扣网刷题
+//水果成篮 II——线段树、数组、二分查找、有序集合、模拟、第440场周赛——简单
+//给你两个长度为 n 的整数数组，fruits 和 baskets，其中 fruits[i] 表示第 i 种水果的 数量，baskets[j] 表示第 j 个篮子的 容量。
+//你需要对 fruits 数组从左到右按照以下规则放置水果：
+//每种水果必须放入第一个 容量大于等于 该水果数量的 最左侧可用篮子 中。
+//每个篮子只能装 一种 水果。
+//如果一种水果 无法放入 任何篮子，它将保持 未放置。
+//返回所有可能分配完成后，剩余未放置的水果种类的数量。
+//示例 1
+//输入： fruits = [4, 2, 5], baskets = [3, 5, 4]
+//输出： 1
+//解释：
+//fruits[0] = 4 放入 baskets[1] = 5。
+//fruits[1] = 2 放入 baskets[0] = 3。
+//fruits[2] = 5 无法放入 baskets[2] = 4。
+//由于有一种水果未放置，我们返回 1。
+//示例 2
+//输入： fruits = [3, 6, 1], baskets = [6, 4, 7]
+//输出： 0
+//解释：
+//fruits[0] = 3 放入 baskets[0] = 6。
+//fruits[1] = 6 无法放入 baskets[1] = 4（容量不足），但可以放入下一个可用的篮子 baskets[2] = 7。
+//fruits[2] = 1 放入 baskets[1] = 4。
+//由于所有水果都已成功放置，我们返回 0。
+//提示：
+//n == fruits.length == baskets.length
+//1 <= n <= 100
+//1 <= fruits[i], baskets[i] <= 1000
+void Countsort(int* nums, int len) {
+	int max = nums[0], min = nums[0];
+	for (int i = 0; i < len; i++) {
+		if (nums[i] > max) {
+			max = nums[i];
+		}
+		else if (nums[i] < min) {
+			min = nums[i];
+		}
+	}
+	int size = max - min + 1;
+	int* hash = (int*)calloc(size, sizeof(int));
+	assert(hash);
+	for (int i = 0; i < len; i++) {
+		int key = nums[i] - min;
+		hash[key] += 1;
+	}
+	for (int i = size - 1, j = 0; i >= 0; i--) {
+		while (hash[i]) {
+			nums[j] = i + min;
+			j += 1;
+			hash[i] -= 1;
+		}
+	}
+	free(hash);
+	hash = NULL;
+}
+int numOfUnplacedFruits(int* fruits, int fruitsSize, int* baskets, int basketsSize) {
+	bool* flag = (bool*)calloc(basketsSize, sizeof(bool));
+	assert(flag);
+	for (int i = 0; i < fruitsSize; i++) {
+		for (int j = 0; j < basketsSize; j++) {
+			if (!flag[j] && fruits[i] <= baskets[j]) {
+				flag[j] = true;
+				break;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < basketsSize; i++) {
+		if (!flag[i]) {
+			ans += 1;
+		}
+	}
+	free(flag);
+	flag = NULL;
+	return ans;
+}
