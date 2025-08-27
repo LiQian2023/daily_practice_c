1cacfb550f7dddb4e14fa383770063b209696fd4
Li_2023-9-13 <1476021453@qq.com>
2025-08-27 22:21:58 +0800

    2025.08.27力扣网刷题 到目标字符串的最短距离——数组、字符串、第325场周赛——简单 方法一：数组遍历

---
+++ 25.08.27_leetcode/25.08.27_leetcode/practice.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+//2025.08.27力扣网刷题
+//到目标字符串的最短距离——数组、字符串、第325场周赛——简单
+//给你一个下标从 0 开始的 环形 字符串数组 words 和一个字符串 target 。环形数组 意味着数组首尾相连。
+//形式上， words[i] 的下一个元素是 words[(i + 1) % n] ，而 words[i] 的前一个元素是 words[(i - 1 + n) % n] ，其中 n 是 words 的长度。
+//从 startIndex 开始，你一次可以用 1 步移动到下一个或者前一个单词。
+//返回到达目标字符串 target 所需的最短距离。如果 words 中不存在字符串 target ，返回 - 1 。
+//示例 1：
+//输入：words = ["hello", "i", "am", "leetcode", "hello"], target = "hello", startIndex = 1
+//输出：1
+//解释：从下标 1 开始，可以经由以下步骤到达 "hello" ：
+//- 向右移动 3 个单位，到达下标 4 。
+//- 向左移动 2 个单位，到达下标 4 。
+//- 向右移动 4 个单位，到达下标 0 。
+//- 向左移动 1 个单位，到达下标 0 。
+//到达 "hello" 的最短距离是 1 。
+//示例 2：
+//输入：words = ["a", "b", "leetcode"], target = "leetcode", startIndex = 0
+//输出：1
+//解释：从下标 0 开始，可以经由以下步骤到达 "leetcode" ：
+//- 向右移动 2 个单位，到达下标 3 。
+//- 向左移动 1 个单位，到达下标 3 。
+//到达 "leetcode" 的最短距离是 1 。
+//示例 3：
+//输入：words = ["i", "eat", "leetcode"], target = "ate", startIndex = 0
+//输出： - 1
+//解释：因为 words 中不存在字符串 "ate" ，所以返回 - 1 。
+//提示：
+//1 <= words.length <= 100
+//1 <= words[i].length <= 100
+//words[i] 和 target 仅由小写英文字母组成
+//0 <= startIndex < words.length
+
+int closestTarget(char** words, int wordsSize, char* target, int startIndex) {
+	int ans = -1;
+	for (int i = 0; i < wordsSize; i++) {
+		int left = (i - startIndex + wordsSize) % wordsSize;
+		int right = (startIndex - i + wordsSize) % wordsSize;
+		int tmp = left < right ? left : right;
+		if (strcmp(words[i], target) == 0) {
+			if (ans == -1 || ans > tmp) {
+				ans = tmp;
+			}
+		}
+	}
+	return ans;
+}
