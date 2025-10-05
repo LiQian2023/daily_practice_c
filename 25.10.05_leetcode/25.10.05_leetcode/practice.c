4583ad58154c8b54e3e7114229cc1a869a8e42a7
Li_2023-9-13 <1476021453@qq.com>
2025-10-05 17:47:13 +0800

    2025.10.05力扣网刷题 最后一块石头的重量——数组、堆（优先队列）、第137场周赛——简单 方法一：排序

---
+++ 25.10.05_leetcode/25.10.05_leetcode/practice.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//2025.10.05力扣网刷题
+//最后一块石头的重量——数组、堆（优先队列）、第137场周赛——简单
+//有一堆石头，每块石头的重量都是正整数。
+//每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+//如果 x == y，那么两块石头都会被完全粉碎；
+//如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y - x。
+//最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。
+//示例：
+//输入：[2, 7, 4, 1, 8, 1]
+//输出：1
+//解释：
+//先选出 7 和 8，得到 1，所以数组转换为[2, 4, 1, 1, 1]，
+//再选出 2 和 4，得到 2，所以数组转换为[2, 1, 1, 1]，
+//接着是 2 和 1，得到 1，所以数组转换为[1, 1, 1]，
+//最后选出 1 和 1，得到 0，最终数组转换为[1]，这就是最后剩下那块石头的重量。
+//提示：
+//1 <= stones.length <= 30
+//1 <= stones[i] <= 1000
+
+int cmp(const void* p1, const void* p2) {
+	return *(int*)p2 - *(int*)p1;
+}
+
+int lastStoneWeight(int* stones, int stonesSize) {
+	if (stonesSize > 1) {
+		for (int i = 0; i < stonesSize; i++) {
+			qsort(stones, stonesSize, sizeof(int), cmp);
+			stones[0] -= stones[1];
+			stones[1] = 0;
+		}
+	}
+	return stones[0];
+}
