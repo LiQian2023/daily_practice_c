3b1502a7a631d08fb19c7f116be1b8e62018cede
Noah <1476021453@qq.com>
2025-12-13 08:21:06 +0800

    2025.12.13力扣网刷题 2765. 最长交替子数组——数组、枚举、第108场双周赛——简单 方法一：双指针

---
+++ 25.12.13_leetcode/25.12.13_leetcode/practice.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+//2025.12.13力扣网刷题
+//2765. 最长交替子数组——数组、枚举、第108场双周赛——简单
+//给你一个下标从 0 开始的整数数组 nums 。如果 nums 中长度为 m 的子数组 s 满足以下条件，我们称它是一个 交替子数组 ：
+//m 大于 1 。
+//s1 = s0 + 1 。
+//下标从 0 开始的子数组 s 与数组[s0, s1, s0, s1, ..., s(m - 1) % 2] 一样。也就是说，s1 - s0 = 1 ，s2 - s1 = -1 ，s3 - s2 = 1 ，s4 - s3 = -1 ，以此类推，直到 s[m - 1] - s[m - 2] = (-1)m 。
+//请你返回 nums 中所有 交替 子数组中，最长的长度，如果不存在交替子数组，请你返回 - 1 。
+//子数组是一个数组中一段连续 非空 的元素序列。
+//示例 1：
+//输入：nums = [2, 3, 4, 3, 4]
+//输出：4
+//解释：交替子数组有[2, 3]，[3, 4]，[3, 4, 3] 和[3, 4, 3, 4]。最长的子数组为[3, 4, 3, 4]，长度为 4。
+//示例 2：
+//输入：nums = [4, 5, 6]
+//输出：2
+//解释：[4, 5] 和[5, 6] 是仅有的两个交替子数组。它们长度都为 2 。
+//提示：
+//2 <= nums.length <= 100
+//1 <= nums[i] <= 10^4
+
+
+int alternatingSubarray(int* nums, int numsSize) {
+	int l = 0, r = 0, ans = -1;
+	while (l < numsSize - 1 && nums[l + 1] - nums[l] != 1) {
+		l += 1;
+	}
+	r = l + 1;
+	int flag = 0;
+	while (r < numsSize) {
+		if (nums[r] - nums[r - 1] != 1 && nums[r] - nums[r - 1] != -1) {
+			if (r - l > 1 && r - l > ans) {
+				ans = r - l;
+			}
+			l = r;
+			flag = 1;
+		}
+		else if (r + 1 < numsSize && nums[r - 1] != nums[r + 1]) {
+			if (r + 1 - l > 1 && r + 1 - l > ans) {
+				ans = r + 1 - l;
+			}
+			l = r;
+			flag = 1;
+		}
+		if (flag) {
+			while (l < numsSize - 1 && nums[l + 1] - nums[l] != 1) {
+				l += 1;
+			}
+			r = l;
+			flag = 0;
+		}
+		r += 1;
+	}
+	if (r - l > 1 && r - l > ans) {
+		ans = r - l;
+	}
+	return ans;
+}
