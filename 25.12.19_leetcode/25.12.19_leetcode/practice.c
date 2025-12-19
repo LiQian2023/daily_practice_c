793c1750c1c15084c0e4765fd816a7361e232901
Noah <1476021453@qq.com>
2025-12-19 13:56:18 +0800

    2025.12.19力扣网刷题 2946. 循环移位后的矩阵相似检查——数组、数学、矩阵、模拟、第373常周赛——简单 方法一：数组遍历

---
+++ 25.12.19_leetcode/25.12.19_leetcode/practice.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+//2025.12.19力扣网刷题
+//2946. 循环移位后的矩阵相似检查——数组、数学、矩阵、模拟、第373常周赛——简单
+//给你一个下标从 0 开始且大小为 m x n 的整数矩阵 mat 和一个整数 k 。请你将矩阵中的 奇数 行循环 右 移 k 次，偶数 行循环 左 移 k 次。
+//如果初始矩阵和最终矩阵完全相同，则返回 true ，否则返回 false 。
+//示例 1：
+//输入：mat = [[1, 2, 1, 2], [5, 5, 5, 5], [6, 3, 6, 3]], k = 2
+//输出：true
+//解释：
+//初始矩阵如图一所示。
+//图二表示对奇数行右移一次且对偶数行左移一次后的矩阵状态。
+//图三是经过两次循环移位后的最终矩阵状态，与初始矩阵相同。
+//因此，返回 true 。
+//示例 2：
+//输入：mat = [[2, 2], [2, 2]], k = 3
+//输出：true
+//解释：由于矩阵中的所有值都相等，即使进行循环移位，矩阵仍然保持不变。因此，返回 true 。
+//示例 3：
+//输入：mat = [[1, 2]], k = 1
+//输出：false
+//解释：循环移位一次后，mat = [[2, 1]] ，与初始矩阵不相等。因此，返回 false 。
+//提示：
+//1 <= mat.length <= 25
+//1 <= mat[i].length <= 25
+//1 <= mat[i][j] <= 25
+//1 <= k <= 50
+
+bool areSimilar(int** mat, int matSize, int* matColSize, int k) {
+	for (int i = 0; i < matSize; i++) {
+		int move = k % matColSize[i];
+		for (int j = 0; j < matColSize[i]; j++) {
+			int col1 = (j + move) % matColSize[i];
+			int col2 = (j - move + matColSize[i]) % matColSize[i];
+			if (i % 2 == 0 && mat[i][j] != mat[i][col1]) {
+				return false;
+			}
+			else if (i % 2 && mat[i][j] != mat[i][col2]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
