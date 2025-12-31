9fd53e150d7e3ab3980f2cf3bd7a030a0173202c
Noah <1476021453@qq.com>
2025-12-31 12:08:42 +0800

    2025.12.31力扣网刷题 3427. 变长子数组求和——数组、前缀和、第433场周赛——简单 方法一：前缀和

---
+++ 25.12.31_leetcode/25.12.31_leetcode/practice.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+//2025.12.31力扣网刷题
+//3427. 变长子数组求和——数组、前缀和、第433场周赛——简单
+//给你一个长度为 n 的整数数组 nums 。对于 每个 下标 i（0 <= i < n），定义对应的子数组 nums[start ... i]（start = max(0, i - nums[i])）。
+//返回为数组中每个下标定义的子数组中所有元素的总和。
+//子数组 是数组中的一个连续、非空 的元素序列。
+//示例 1：
+//输入：nums = [2, 3, 1]
+//输出：11
+//解释：
+//下标 i	子数组	和
+//0	nums[0] = [2]	2
+//1	nums[0 ... 1] = [2, 3]	5
+//2	nums[1 ... 2] = [3, 1]	4
+//总和	 	11
+//总和为 11 。因此，输出 11 。
+//示例 2：
+//输入：nums = [3, 1, 1, 2]
+//输出：13
+//解释：
+//下标 i	子数组	和
+//0	nums[0] = [3]	3
+//1	nums[0 ... 1] = [3, 1]	4
+//2	nums[1 ... 2] = [1, 1]	2
+//3	nums[1 ... 3] = [1, 1, 2]	4
+//总和	 	13
+//总和为 13 。因此，输出为 13 。
+//提示：
+//1 <= n == nums.length <= 100
+//1 <= nums[i] <= 1000
+
+int subarraySum(int* nums, int numsSize) {
+	int ans = 0;
+	int* prefix = (int*)calloc(numsSize + 1, sizeof(int));
+	assert(prefix);
+	for (int i = 0; i < numsSize; i++) {
+		prefix[i + 1] = prefix[i] + nums[i];
+		int start = i - nums[i] > 0 ? i - nums[i] : 0;
+		ans += prefix[i + 1] - prefix[start];
+	}
+	free(prefix);
+	prefix = NULL;
+	return ans;
+}
