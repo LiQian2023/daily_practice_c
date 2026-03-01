05bf2a7dd234108e45b6e109157bf746338e888b
Noah <1476021453@qq.com>
2026-03-02 02:53:28 +0800

    2026.03.02力扣网刷题 589. N 叉树的前序遍历——栈、树、深度优先搜索——简单 方法一：先序遍历

---
+++ 26.03.02_leetcode/26.03.02_leetcode/practice.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+//2026.03.02力扣网刷题
+//589. N 叉树的前序遍历——栈、树、深度优先搜索——简单
+//给定一个 n 叉树的根节点  root ，返回 其节点值的 前序遍历 。
+//n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
+//示例 1：
+//输入：root = [1, null, 3, 2, 4, null, 5, 6]
+//输出：[1, 3, 5, 6, 2, 4]
+//示例 2：
+//输入：root = [1, null, 2, 3, 4, 5, null, null, 6, 7, null, 8, null, 9, 10, null, null, 11, null, 12, null, 13, null, null, 14]
+//输出：[1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10]
+//提示：
+//节点总数在范围[0, 10^4]内
+//0 <= Node.val <= 10^4
+//n 叉树的高度小于或等于 1000
+//进阶：递归法很简单，你可以使用迭代法完成此题吗 ?
+
+struct Node {
+    int val;
+    int numChildren;
+    struct Node** children;
+};
+typedef struct Node NTN;
+
+int getSize(NTN* root) {
+    if (!root) {
+        return 0;
+    }
+    if (root->numChildren == 0) {
+        return 1;
+    }
+    int size = 0;
+    for (int i = 0; i < root->numChildren; i++) {
+		size += getSize(root->children[i]);
+    }
+    return size + 1;
+}
+void dfs(NTN* root, int* ans, int* pi) {
+    if (!root) {
+        return;
+    }
+    ans[*pi] = root->val;
+    *pi += 1;
+    for (int i = 0; i < root->numChildren; i++) {
+        dfs(root->children[i], ans, pi);
+    }
+}
+int* preorder(struct Node* root, int* returnSize) {
+	*returnSize = getSize(root);
+    int* ans = (int*)calloc(*returnSize, sizeof(int));
+    assert(ans);
+    int i = 0;
+    dfs(root, ans, &i);
+    return ans;
+}
