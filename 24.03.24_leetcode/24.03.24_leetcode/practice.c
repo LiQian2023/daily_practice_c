808a517c087de12b9989592e31b43187ff64694e
Noah <1476021453@qq.com>
2026-04-14 02:37:29 +0800

    2024.03.24力扣网刷题 2026.04.14完成解答并通过力扣所有测试用例 方法一：动态规划

--- 24.03.24_leetcode/24.03.24_leetcode/practice.c
+++ 24.03.24_leetcode/24.03.24_leetcode/practice.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
-
+#include <stdlib.h>
 //2024.03.24力扣网刷题
 //反转字符串——双指针、字符串——简单
 //编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
@@ -26,7 +26,7 @@ void reverseString(char* s, int sSize) {
 }
 
 //2024.03.24力扣网刷题
-// 2024.完成解答并通过力扣所有测试用例
+//2026.04.14完成解答并通过力扣所有测试用例
 //零钱兑换——广度优先搜索、数组、动态规划——中等
 //给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
 //计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 - 1 。
@@ -46,27 +46,29 @@ void reverseString(char* s, int sSize) {
 //1 <= coins[i] <= 2^31 - 1
 //0 <= amount <= 10^4
 
-int cmp(const void* p1, const void* p2) {
-	return *(int*)p1 - *(int*)p2;
-}
-int coinChange(int* coins, int coinsSize, int amount) {
-	qsort(coins, coinsSize, sizeof(int), cmp);
-	if (amount == 0)
-		return 0;
-	int count = 0;
-	int i = coinsSize - 1;
-	int tmp = amount;
-	while (tmp && i) {
-		if (tmp - coins[i] < 0)
-			i--;
-		else {
-			tmp -= coins[i];
-			count++;
-		}
-		if (tmp < 0) {
 
+int coinChange(int* coins, int coinsSize, int amount) {
+	long long* dp = (long long*)calloc(amount + 1, sizeof(long long));
+	assert(dp);
+	for (int i = 1; i <= amount; i++) {
+		dp[i] = INT_MAX;
+	}
+	for (int i = 0; i <= amount; i++) {
+		for (int j = 0; j < coinsSize; j++) {
+			int tmp = i - coins[j];
+			dp[i] = (tmp >= 0 ? min(dp[i], dp[tmp] + 1) : dp[i]);
 		}
 	}
-	return !amount ? count : -1;
+	int ans = dp[amount] == INT_MAX ? -1 : dp[amount];
+	free(dp);
+	return ans;
 }
-
+// 状态转移方程
+// [ 3,   5, 7] =11
+// [ 4,   3, ]
+// dp[0],dp[1]
+// [0,-1,-1,1,-1,1,2,]
+// 7->4
+// 5->6->3->3
+// dp[n] = dp[n - 1] + amount - dp[n - 1]
+// 
