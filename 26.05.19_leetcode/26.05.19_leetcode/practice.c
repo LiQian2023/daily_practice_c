42addf61352aeddbee20aa01d76aab1ec8955394
Noah <1476021453@qq.com>
2026-05-19 23:05:46 +0800

    2026.05.19力扣网刷题 2540. 最小公共值——中级工程师、数组、哈希表、双指针、二分查找、第96场双周赛——简单 方法一：双指针

---
+++ 26.05.19_leetcode/26.05.19_leetcode/practice.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+//2026.05.19力扣网刷题
+//2540. 最小公共值——中级工程师、数组、哈希表、双指针、二分查找、第96场双周赛——简单
+//给你两个整数数组 nums1 和 nums2 ，它们已经按非降序排序，请你返回两个数组的 最小公共整数 。如果两个数组 nums1 和 nums2 没有公共整数，请你返回 - 1 。
+//如果一个整数在两个数组中都 至少出现一次 ，那么这个整数是数组 nums1 和 nums2 公共 的。
+//示例 1：
+//输入：nums1 = [1, 2, 3], nums2 = [2, 4]
+//输出：2
+//解释：两个数组的最小公共元素是 2 ，所以我们返回 2 。
+//示例 2：
+//输入：nums1 = [1, 2, 3, 6], nums2 = [2, 3, 4, 5]
+//输出：2
+//解释：两个数组中的公共元素是 2 和 3 ，2 是较小值，所以返回 2 。
+//提示：
+//1 <= nums1.length, nums2.length <= 10^5
+//1 <= nums1[i], nums2[j] <= 10^9
+//nums1 和 nums2 都是 非降序 的。
+
+int getCommon(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+	int ans = -1;
+	for (int i = 0, j = 0; i < nums1Size && j < nums2Size;) {
+		if (nums1[i] == nums2[j]) {
+			ans = nums1[i];
+			break;
+		}
+		else if (nums1[i] < nums2[j]) {
+			i += 1;
+		}
+		else if (nums1[i] > nums2[j]) {
+			j += 1;
+		}
+	}
+	return ans;
+}
