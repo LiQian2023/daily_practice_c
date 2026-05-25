05e7079ba427796fbe67ade1a91d529bd3f97702
Noah <1476021453@qq.com>
2026-05-26 03:07:29 +0800

    2026.05.26力扣网刷题 3120. 统计特殊字母的数量 I——中级工程师、哈希表、字符串、第394场周赛——简单 方法一：哈希表

---
+++ 26.05.26_leetcode/26.05.26_leetcode/practice.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+//2026.05.26力扣网刷题
+//3120. 统计特殊字母的数量 I——中级工程师、哈希表、字符串、第394场周赛——简单
+//给你一个字符串 word。如果 word 中同时存在某个字母的小写形式和大写形式，则称这个字母为 特殊字母 。
+//返回 word 中 特殊字母 的数量。
+//示例 1:
+//输入：word = "aaAbcBC"
+//输出：3
+//解释：
+//word 中的特殊字母是 'a'、'b' 和 'c'。
+//示例 2:
+//输入：word = "abc"
+//输出：0
+//解释：
+//word 中不存在大小写形式同时出现的字母。
+//示例 3:
+//输入：word = "abBCab"
+//输出：1
+//解释：
+//word 中唯一的特殊字母是 'b'。
+//提示：
+//1 <= word.length <= 50
+//word 仅由小写和大写英文字母组成。
+
+int numberOfSpecialChars(char* word) {
+	int hash[26] = { 0 };
+	for (int i = 0; word[i]; i++) {
+		int key = word[i] - 'a';
+		if (isupper(word[i])) {
+			key = word[i] - 'A';
+			if (hash[key] < 2) {
+				hash[key] += 2;
+			}
+		}
+		else {
+			if (hash[key] == 0 || hash[key] == 2) {
+				hash[key] += 1;
+			}
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < 26; i++) {
+		if (hash[i] == 3) {
+			ans += 1;
+		}
+	}
+	return ans;
+}
